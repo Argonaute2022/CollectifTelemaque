05cfbfce454f652cb6084dc39bb0a09ba46f74ed
Benjamin <ben@amaunet-2.home>
2023-07-17 07:58:05 +0200

    MàJ liste tribunes

--- src/contents/fr.tsx
+++ src/contents/fr.tsx
@@ -109,6 +109,7 @@ Des tribunes dans la presse
 * [Ne pas avoir mis de garde-fous pour empêcher le RN de voter la motion de censure de la Nupes fut une erreur](https://www.lemonde.fr/idees/article/2022/11/08/ne-pas-avoir-mis-de-garde-fous-pour-empecher-le-rn-de-voter-la-motion-de-censure-de-la-nupes-fut-une-erreur_6148988_3232.html) (Le Monde, 08/11/2022)
 * [Un Parlement qui fonctionne](https://www.telos-eu.com/fr/politique-francaise-et-internationale/un-parlement-qui-fonctionne.html) (Telos, 07/02/2023)
 * [La gauche et l’Europe: et si on regardait du côté allemand?](https://www.telos-eu.com/fr/politique-francaise-et-internationale/la-gauche-et-leurope-et-si-on-regardait-du-cote-al.html) (Telos, 20/02/2023)
+* [Pour sauver la planète, regardons… le passé](https://lejournal.info/article/pour-sauver-la-planete-regardons-le-passe/) (Le Journal, 17/07/2023)
 
 Des manifestes collectifs
 
